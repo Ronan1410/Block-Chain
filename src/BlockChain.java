2227764677cab33b69c579e9f4372d8584df7420
Ronan1410 <1410ronan@gmail.com>
2025-09-13 16:27:05 +0530

    The Main BlockChain Program

---
+++ src/BlockChain.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import com.google.gson.GsonBuilder;
+
+public class BlockChain
+{
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+    public static int difficulty = 5;  // taken 5 to test
+
+    public static void main(String[] args)
+    {
+        blockchain.clear();
+        blockchain.add(new Block("Block 1","0"));
+        System.out.println("Trying to mine the first block");
+        blockchain.get(0).mineBlock(difficulty);
+
+        blockchain.add(new Block("Block 2",blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to mine the second block");
+        blockchain.get(1).mineBlock(difficulty);
+
+        blockchain.add(new Block("Block 3",blockchain.get(blockchain.size()-2).hash));
+        System.out.println("Trying to mine the third block");
+        blockchain.get(2).mineBlock(difficulty);
+
+        System.out.println("\nBlcokchain Validity: "+isValid());
+
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println("The BlockChain: ");
+        System.out.println(blockchainJson);
+    }
+
+    public static boolean isValid()
+    {
+        Block currentBlock;
+        Block previousBlock;
+        for(int i=1;i<blockchain.size();i++)
+        {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i-1);
+
+            if(!currentBlock.hash.equals(previousBlock.calculateHash()))
+            {
+                System.out.println("The hashes are not equal");
+                return false;
+            }
+
+            if(!previousBlock.hash.equals(currentBlock.previous_hash))
+            {
+                System.out.println("The previous hashes are not equal");
+                return false;
+            }
+        }
+        return true;
+    }
+}
