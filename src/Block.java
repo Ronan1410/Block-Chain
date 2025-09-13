9d09cd9076f83b66293669a56bde9c2a18a978de
Ronan1410 <1410ronan@gmail.com>
2025-09-13 16:25:46 +0530

    Block

--- src/Block.java
+++ src/Block.java
@@ -5,12 +5,31 @@ public class Block
     public String previous_hash;
     private String data;
     private long time_data;
+    int nonce;
 
     public Block(String data, String previous_hash)
     {
         this.data = data;
         this.previous_hash = previous_hash;
         this.time_data = new Date().getTime();
+
+        this.hash=calculateHash();
     }
 
+    public String calculateHash()
+    {
+        String caclculated_hash = StringUtil.applySSha256(previous_hash + Long.toString(time_data) + Integer.toString(nonce) + data);
+        return caclculated_hash;
+    }
+
+    public void mineBlock(int difficulty)
+    {
+        String target =  new String(new char[difficulty]).replace('\0', '0');
+        while(!hash.substring(0, difficulty).equals(target))
+        {
+            nonce ++;
+            hash= calculateHash();
+        }
+        System.out.println("Block Mined: "+ hash);
+    }
 }
